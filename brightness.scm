c89b9c0d6e464d34716317d8cffa13e6f2a54a1e
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-11-01 22:39:33 +0200

    Removed extraneous local

--- brightness.scm
+++ brightness.scm
@@ -8,24 +8,22 @@
    (car
     (string-split string))))
 
-
 (define max-brightness-path
   "/sys/class/backlight/mba6x_backlight/max_brightness")
 (define brightness-path
   "/sys/class/backlight/mba6x_backlight/brightness")
 
 (define (brightness op)
-  (let ((operation op)
-	(max-brightness (to-number (read-all max-brightness-path)))
+  (let ((max-brightness (to-number (read-all max-brightness-path)))
 	(brightness (to-number (read-all brightness-path)))
 	(change (lambda (x) (with-output-to-file brightness-path (lambda () (format #t "~A~%" x))))))
-    (cond ((= brightness max-brightness) (if (eq? operation +)
+    (cond ((= brightness max-brightness) (if (eq? op +)
 					    'maximum
-					    (change (operation brightness 10))))
-	  ((= brightness 0) (if (eq? operation -)
+					    (change (op brightness 10))))
+	  ((= brightness 0) (if (eq? op -)
 				'minimum
-				(change (operation brightness 10))))
-	  (else (change (operation brightness 10))))))
+				(change (op brightness 10))))
+	  (else (change (op brightness 10))))))
 
 (define (inc) (brightness +))
 (define (dec) (brightness -))
