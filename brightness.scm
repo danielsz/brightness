8eb29d821d0e479abf99783b4e3585da7da17cf3
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2019-01-23 16:02:13 +0200

    Chicken Scheme 5

--- brightness.scm
+++ brightness.scm
@@ -1,29 +1,38 @@
 #! /usr/bin/csi -script
 (require-extension shell)
-(use posix)
-
+(import (chicken file posix)
+	(chicken process-context)
+	(chicken port)
+	(chicken io)
+	(chicken string)
+	(chicken format)
+	args)
 
 (define (to-number string)
   (string->number 
    (car
     (string-split string))))
 
+(define (read-all file)
+  (call-with-input-file file (lambda (port) (read-string #f port))))
+
 (define max-brightness-path
-  "/sys/class/backlight/mba6x_backlight/max_brightness")
+  "/sys/class/backlight/intel_backlight/max_brightness")
 (define brightness-path
-  "/sys/class/backlight/mba6x_backlight/brightness")
+  "/sys/class/backlight/intel_backlight/brightness")
 
 (define (brightness op)
-  (let ((max-brightness (to-number (read-all max-brightness-path)))
+  (let ((lumen 500)
+	(max-brightness (to-number (read-all max-brightness-path)))
 	(brightness (to-number (read-all brightness-path)))
 	(change (lambda (x) (with-output-to-file brightness-path (lambda () (format #t "~A~%" x))))))
     (cond ((= brightness max-brightness) (if (eq? op +)
 					    'maximum
-					    (change (op brightness 10))))
+					    (change (op brightness lumen))))
 	  ((= brightness 0) (if (eq? op -)
 				'minimum
-				(change (op brightness 10))))
-	  (else (change (op brightness 10))))))
+				(change (op brightness lumen))))
+	  (else (change (op brightness lumen))))))
 
 (define (inc) (brightness +))
 (define (dec) (brightness -))
